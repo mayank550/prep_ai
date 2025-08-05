42283ca89f8f4e459f54e30c384cdbcff5048300
mayank <mayankhere567@gmail.com>
2025-08-05 19:07:07 +0530

    Intial:Project intiated and setup completed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className="text-blue-500">
+       <Button variant={"destructive"}>click me</Button>
+    </div>
+  )
+}
+
+export default App
+
